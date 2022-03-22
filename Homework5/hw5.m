88ecb7866fb90c0dad74d0fc3bf67d4fadb966e3
Ethan Shealey <ethan.shealey@gmail.com>
2022-03-22 18:24:48 -0400

    Adding HW5

--- Homework5/hw5.m
+++ Homework5/hw5.m
@@ -5,55 +5,42 @@
     The purpose of this program is to create a 
     simple feedfoward neural network that takes in
 %}
-%{
-function SSE = hw5(in, t)
-    inputs = load(in);
-    targets = load(t);
 
-    inputsLength = length(inputs.bodyfatInputs);
-    targetsLength = length(targets.bodyfatTargets);
-
-    randomSplit = randi(inputsLength);
-
-    trainInputs = inputs.bodyfatInputs(:, 1:randomSplit);
-    trainTargets = targets.bodyfatTargets(:, 1:randomSplit);
-
-    testInputs = inputs.bodyfatInputs(:, randomSplit+1:end);
-    testTargets = targets.bodyfatTargets(:, randomSplit+1:end);
-
-    net = feedforwardnet(2);
-    disp('Training...')
-    net = train(net, trainInputs, trainTargets);
-
-    out = net(double(testInputs));
-
-    disp('Testing...')
-    SSE = perform(net, testTargets, out);
-end
-%}
 
 function avgSSE = hw5()
+
     clc
     close all
     clear
 
+    % Load in the bodyfat data
     load('bodyfat_dataset.mat');
+
+    % Set the training function as trainlm
     trainFcn = 'trainlm';
+
+    % Create the feedfoward neural network
     net = feedforwardnet([100,100], trainFcn);
 
+    % Pull the inputs and targets from dataset
     x = bodyfatInputs
     y = bodyfatTargets
 
+    % Train the nueral network
     [net,tr] = train(net,x,y);
     id = 80
 
+    % Pull the test data from dataset
     test_data = x(:,id);
     test_target = y(:,id);
 
+    % Test the neural network
     guess = net(test_data);
 
+    % Calculate the error
     SSE = perform(net, test_target, guess);
 
+    % Calculate the average error
     avgSSE = SSE/length(test_target);
 
 end
